1f69ff5645770790ed79fd3c2859cbded44fdaa4
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-09-28 21:33:25 -0300

    :wrench: chore: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryWhite: '#F5F5F5',
+        campusRed: '#6C0318',
+        black: '#000',
+        red: '#FF0004',
+        blue: '#00509A',
+        green:'#008810',
+        purple: '#2800F2'
+      },
+      fontFamily: {
+        pregular: 'Palanquin-Regular',
+        psemibold: 'Palanquin-SemiBold',
+        pbold: 'Palanquin-Bold'
+      }
+    },
+  },
+  plugins: [],
+}
+

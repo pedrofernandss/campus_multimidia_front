0743b441c0912d57cef2f59d73c37c45f75eac9d
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-09-28 21:34:30 -0300

    :lipstick: feat: config image and fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pbold">Olá, esse é o aplicativo do Campus.</Text>
+      <StatusBar style='auto'/>
+      <Link href="/profile" style={{ color: 'blue' }}>Go to profile</Link>
     </View>
   );
 }
